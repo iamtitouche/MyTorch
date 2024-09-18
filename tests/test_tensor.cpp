6c78246623f107a66460eb300129b19714b20280
iamtitouche <titouan.sadoun2002@gmail.com>
2024-09-18 12:23:43 +0200

    Adding tests directory

---
+++ tests/test_tensor.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+#include "tensor.h"
+
+// Test fixture class
+class TensorTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        // This function is called before each test
+    }
+
+    void TearDown() override {
+        // This function is called after each test
+    }
+
+    // Example tensor shapes for testing
+    tensorShape shape1 = tensorShape({2, 3});
+    tensorShape shape2 = tensorShape({3, 2});
+};
+
+// Test constructor with shape
+TEST_F(TensorTest, ConstructorWithShape) {
+    tensor t(shape1);
+    EXPECT_EQ(t.sameShape(tensor(shape1)), true);
+    EXPECT_EQ(t.sameSize(tensor(shape1)), true);
+    EXPECT_EQ(t.getShape(), shape1);
+}
+
+// Test constructor with shape and initial value
+TEST_F(TensorTest, ConstructorWithShapeAndValue) {
+    double initValue = 5.0;
+    tensor t(shape1, initValue);
+    EXPECT_EQ(t.sameShape(tensor(shape1)), true);
+    EXPECT_EQ(t.sameSize(tensor(shape1)), true);
+}
